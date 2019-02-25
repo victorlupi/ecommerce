37d0d6ff5bc8e04fbabee3d41d3ed45856f9f3b3
victorlupi <victor.lupi@hotmail.com>
2019-02-25 14:57:17 -0300

    criamos pageadmin e tpl admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");//do composer
 
 use \Slim\Slim;//namespace
 use \Hcode\page;//namespace
+use \Hcode\pageadmin;//namespace
 
 $app = new \Slim\Slim();//por causa das rotas para facilitar (ajuda no SEO)
 
@@ -12,6 +13,14 @@ $app->config('debug', true);
 $app->get('/', function() {
 
 	$page = new page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+
+	$page = new pageadmin();
 
 	$page->setTpl("index");
 
